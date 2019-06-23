2bd9c39a5dea4c5a0eaef42a7e2a206eaf42536f
Fritz Lin <uxfritz@163.com>
2019-06-23 18:17:11 +0800

    add AppRouter via react-router-dom

---
+++ src/AppRouter.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from "react";
+import About from './About';
+import App from './App';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Route path="/" exact component={App} />
+      <Route path="/about" component={About} />
+    </Router>
+  );
+}
+
+export default AppRouter;
